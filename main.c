1bd5d9ac88b520818ee4ff6599dfd2f856bc1378
Jussi Timonen <jussi.timonen@houston-inc.com>
2020-01-16 02:49:43 +0200

    Fixed encodenumber loop

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 #include "vbyte.h"
 
 int main() {
-  u_list* ulist = generate_random_u_list(100);
+  u_list* ulist = generate_random_u_list(10);
   print_u_list(ulist);
-  c_list* test = vb_encode_number(ulist->first->val);
-  print_c_list(test);
+//  print_c_list(test);
+  printf("<encode+decode>\n");
   c_list* test2 = vb_encode(ulist);
-  print_c_list(test2);
+//  print_c_list(test2);
+  u_list* test3 = vb_decode(test2);
+  print_u_list(test3);
   return 0;
 }
