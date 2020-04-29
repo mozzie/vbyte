c44294774b386f31d98b22ea844be187b3728f0a
Jussi Timonen <jussi.timonen@houston-inc.com>
2020-04-30 00:21:32 +0300

    modified generator

--- generate.cpp
+++ generate.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
   if(argc<=2) {
-    cout  << "Usage: generate <uint64_t count> <number of bits in number>" << endl;
+    cout  << "Usage: generate <uint64_t count> <name>" << endl;
     exit(1);
   }
   int amount = atoi(argv[1]);
   if(amount == 0) {
     exit(1);
   }
-  int max = atoi(argv[2]);
+
   random_device rd;
   mt19937_64 gen(rd());
   uniform_int_distribution<unsigned long long> dis;
@@ -23,14 +23,12 @@ int main(int argc, char *argv[]) {
   filename.append(argv[2]);
    int maxvalues [8] = {1<<7,1<<8,1<<15,1<<16,1<<23,1<<24,1<<30,1<<30}; // <- all
   // int maxvalues [8] = {1<<7,1<<7,1<<7,1<<8,1<<8,1<<8,1<<16,1<<31}; // <- twolarge
-  // int maxvalues [8] = {1<<3,1<<4,1<<5,1<<6,1<<7,1<<8,1<<16,1<<31}; // <- vsmall
   //int maxvalues [8] = {1<<2,1<<2,1<<3,1<<3,1<<3,1<<4,1<<4,1<<15}; // <- onelarge
-  //int maxvalues [8] = {1<<2,1<<2,1<<3,1<<3,1<<3,1<<4,1<<4,1<<4}; // <- onebyte
+  //int maxvalues [8] = {1<<2,1<<2,1<<3,1<<3,1<<3,1<<4,1<<4,1<<4}; // <- onlysmall
   ofstream file(filename, ios::out | ios::binary);
   for (int n=0; n<amount; ++n) {
     int rand = dis(gen)%8;
     uint64_t i = dis(gen)%maxvalues[rand];
-//    uint64_t i = dis(gen)%max;
     file.write(reinterpret_cast<const char *>(&i), sizeof(uint64_t));
   }
   file.close();
